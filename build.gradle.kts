1c1da947524496bc217f98698018021cfc7c162a
woshiyizi <52490707+woshiyizi@users.noreply.github.com>
2026-02-08 17:44:15 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.agp.app) apply false
-    alias(libs.plugins.agp.lib) apply false
     alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.kotlinSerialization) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.jetbrains.compose) apply false
-}
+    alias(libs.plugins.ksp) apply false
+    
+    // ❌ 删除下面这一行 (因为你删除了 agp-lib 的定义)
+    // alias(libs.plugins.agp.lib) apply false 
 
-tasks.register<Delete>("clean") {
-    delete(layout.buildDirectory)
+    // ❌ 删除下面这一行 (因为你删除了 jetbrains-compose 的定义)
+    // alias(libs.plugins.jetbrains.compose) apply false
+
+    // 保留其他插件 (如果有的话，例如 lsplugin)
+    alias(libs.plugins.lsplugin.apksign) apply false
+    alias(libs.plugins.lsplugin.resopt) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.parcelize) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
